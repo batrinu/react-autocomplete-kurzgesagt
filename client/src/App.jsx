2514055c11ee9bd3b85b144c69a47ad11dbe0f66
batrinu <batrinu@outlook.com>
2018-04-18 16:28:58 +0300

    basic match function
    still a little buggy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,34 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      countryList: ['Albania', 'Colombia', 'Romania', 'Algeria', 'Belgium', 'France', 'Spain', 'Norway', 'Greece']
+      countryList: ['Albania', 'Colombia', 'Romania', 'Algeria', 'Belgium', 'France', 'Spain', 'Norway', 'Greece'],
+      matches: []
     }
+    this.handleSearchInputChange = this.handleSearchInputChange.bind(this)
+    this.getMatches = this.getMatches.bind(this)
+  }
+
+  handleSearchInputChange(event) {
+    if(event.target.value.length > 0) {
+      let matches = this.getMatches(event.target.value)
+      this.setState({
+        matches: matches
+      })
+    }
+  }
+
+  getMatches(inputText) {
+    let matchList = []
+    for(var i = 0; i< this.state.countryList.length; i++) {
+      if (this.state.countryList[i].toLowerCase().indexOf(inputText.toLowerCase()) != -1) {
+        matchList.push(this.state.countryList[i])
+      }
+    }
+    return matchList
+  }
+
+  displayMatches() {
+
   }
 
   componentDidMount() {
@@ -14,23 +40,28 @@ class App extends React.Component {
   }
 
   render() {
+    const { matches } = this.state
+    const listItems = matches.map((match)=>{
+      return <li>{match}</li>
+    })
     return (
       <BrowserRouter>
         <main className="container">
           <form id='search-form'>
-            <h1>Basic autocomplete example</h1>
-            <div className='search-input-container'>
+            <h1 className='Title'>Basic autocomplete example</h1>
+            <div className='Input'>
               <input
                 type='text'
                 placeholder='search'
-                id='search-input'
+                className='Input-text'
                 ref={(input) => { this.searchInput = input; }}
+                onChange = {this.handleSearchInputChange}
               />
-              <label>Search box</label>
+              <label className='Input-label'>Search box</label>
 
             </div>
           </form>
-          <ul id='search-results'></ul>
+          <ul id='search-results'>{listItems}</ul>
         </main>
       </BrowserRouter>
     )
